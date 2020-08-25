57b9de5b6540f7cb3a431867ec2ad1f88d1f9fc6
ArikMackenburg <Arikmackenburg@gmail.com>
2020-08-25 15:01:37 -0500

    Shift.cs and Shift.Test.cs

---
+++ Challenges/Arrays/Shift.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenges.Arrays
+{
+    public class Shift
+    {
+        public static int[] ShiftArray(int[] array, int num)
+        {
+            int[] shiftedArray = new int[array.Length + 1];
+            int mid = shiftedArray.Length / 2;
+            for (int i = 0; i < mid; i++)
+            {
+                shiftedArray[i] = array[i];
+            }
+            shiftedArray[mid] = num;
+            for (int i = mid+1; i < shiftedArray.Length; i++ )
+            {
+                shiftedArray[i] = array[i - 1];
+            }
+            return shiftedArray;
+        }
+    }
+}
