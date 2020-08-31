395ba55d52ddf95d6f03612ee338217b5455bbc3
ArikMackenburg <Arikmackenburg@gmail.com>
2020-08-31 15:48:11 -0500

    Tests passing

--- DataStructures.Tests/LinkedLists/LinkedListTest.cs
+++ DataStructures.Tests/LinkedLists/LinkedListTest.cs
@@ -48,6 +48,52 @@ namespace DataStructures.Tests.LinkedLists
             Assert.Equal(expect, list.Includes(val));
 
         }
-        
+
+        [Fact]
+        public void Append_adds_value_to_end()
+        {
+            LinkedList list = new LinkedList();
+
+            list.Append(1);
+
+            Assert.Equal("{ 1 } -> NULL", list.ToString());
+
+            list.Append(2);
+
+            Assert.Equal("{ 1 } -> { 2 } -> NULL", list.ToString());
+        }
+
+        [Theory]
+        [InlineData(2, 5, "{ 1 } -> { 5 } -> { 2 } -> { 3 } -> NULL")]
+        [InlineData(4, 5, "{ 1 } -> { 2 } -> { 3 } -> NULL")]
+        public void Insert_before_inserts_before(int value, int newValue, string expect)
+        {
+            LinkedList list = new LinkedList();
+            list.Append(1);
+            list.Append(2);
+            list.Append(3);
+
+            list.InsertBefore(value, newValue);
+
+            Assert.Equal(expect, list.ToString());
+
+        }
+
+        [Theory]
+        [InlineData(2, 5, "{ 1 } -> { 2 } -> { 5 } -> { 3 } -> NULL")]
+        [InlineData(4, 5, "{ 1 } -> { 2 } -> { 3 } -> NULL")]
+        public void Insert_after_inserts_after(int value, int newValue, string expect)
+        {
+            LinkedList list = new LinkedList();
+            list.Append(1);
+            list.Append(2);
+            list.Append(3);
+
+            list.InsertAfter(value, newValue);
+
+            Assert.Equal(expect, list.ToString());
+
+        }
+
     }
 }
