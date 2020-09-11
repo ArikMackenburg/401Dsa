3284350be31324628032407f8919e624dc307caf
ArikMackenburg <Arikmackenburg@gmail.com>
2020-09-11 00:37:24 -0500

    passing first test i know there is more to the solution but its bed time

---
+++ Challenges/StacksQueues/MultiBracket.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using DataStructures.StacksQueues;
+using System.Text;
+
+namespace Challenges.StacksQueues
+{
+    public class MultiBracket
+    {
+        Stack<char> brackets = new Stack<char>();
+
+        public bool CheckBrackets(string input)
+        {
+            
+            foreach (char c in input)
+            {
+                if(CheckLeftBracket(c) == true )
+                {
+                    brackets.Push(c);
+                }
+                if(CheckRightBracket(c) == true )
+                {
+                    if (MatchCheker(c) != true)
+                    {
+                        return false;
+                    }
+                    continue;
+                }
+                continue;
+            }
+            return true;
+        }
+        public bool CheckLeftBracket(char c)
+        {
+            if (c == '(' || c == '{' || c == '[')
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool CheckRightBracket(char c)
+        {
+            if (c == ')' || c == '}' || c == ']')
+            {
+                return true;
+            }
+            return false;
+        }
+       
+
+        public bool MatchCheker(char type)
+        {
+            char c;
+
+            if (type == ')')
+            {
+                c = '(';
+                return Helper(c);
+            }
+                
+            if (type == '}')
+            {
+                c = '{';
+                return Helper(c);
+            }
+
+            if (type == ']')
+            {
+                c = '[';
+                return Helper(c);
+            }
+            return false;
+
+        }
+
+        public bool Helper(char c)
+        {
+            if (brackets.Pop() == c)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    
+}
---
+++ Tests/Challenges.Test/StacksQueues/MultiBracketTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Challenges.StacksQueues;
+using DataStructures.StacksQueues;
+
+namespace Challenges.Test.StacksQueues
+{
+    public class MultiBracketTest
+    {
+        [Fact]
+        public void Returns_coorect()
+        {
+            MultiBracket multiBracket = new MultiBracket();
+            Assert.True(multiBracket.CheckBrackets("[]"));
+
+        }
+    }
+}
