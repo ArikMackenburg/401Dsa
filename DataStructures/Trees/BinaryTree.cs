0a1ca0d384c3bb1562c495d2bef37c42f09b0daf
ArikMackenburg <Arikmackenburg@gmail.com>
2020-09-14 09:07:15 -0500

    stuff

--- DataStructures/Trees/BinaryTree.cs
+++ DataStructures/Trees/BinaryTree.cs
@@ -1,14 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
+using DataStructures.StacksQueues;
 
 namespace DataStructures.Trees
 {
     public class BinaryTree<T>
     {
-      
-       
-       
+        public TreeNode<T> Root { get; set; }
+
+        public Queue<T> Q = new Queue<T>();
+        public Stack<T> Stack { get; set; }
+
+        //public T[] PreOrder()
+        //{
+            
+            
+        //}
 
 
     }
