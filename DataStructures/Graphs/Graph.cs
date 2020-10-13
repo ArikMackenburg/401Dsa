32a9ee07bc48b2d63cdee4c89cbe7bd16b6cafaf
ArikMackenburg <Arikmackenburg@gmail.com>
2020-10-13 00:11:57 -0500

    got my ass kicked some but might be working now?

--- DataStructures/Graphs/Graph.cs
+++ DataStructures/Graphs/Graph.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DataStructures.Graphs
@@ -8,10 +9,11 @@ namespace DataStructures.Graphs
         where T : IComparable
     {
         public List<Vertex<T>> Vertices { get; } = new List<Vertex<T>>();
+        
 
-        public void AddNode(T value)
+        public void AddVertex(T value)
         {
-            if (Vertices.Contains(this[value]))
+            if (this[value] != null )
                 throw new Exception();
 
             var vertex = new Vertex<T>(value);
@@ -19,10 +21,21 @@ namespace DataStructures.Graphs
         }
         public void AddEdge(T value1, T value2)
         {
-            if(!this[value1].Neighbors.Contains(this[value2]))
-                this[value1].Neighbors.Add(this[value2]);
-            if (!this[value2].Neighbors.Contains(this[value1]))
-                this[value2].Neighbors.Add(this[value1]);
+            var vertex1 = this[value1];
+            if (vertex1 == null)
+            {
+                throw new Exception();
+            }
+            var vertex2 = this[value2];
+            if (vertex2 == null)
+            {
+                throw new Exception();
+            }
+
+            if (!vertex1.Neighbors.Contains(vertex2))
+                vertex1.Neighbors.Add(vertex2);
+            if (!vertex2.Neighbors.Contains(vertex1))
+                vertex2.Neighbors.Add(vertex1);
         }
         public IEnumerable<Vertex<T>> GetNeighbors(T value)
         {
@@ -50,5 +63,35 @@ namespace DataStructures.Graphs
         }
         
         public Vertex<T> this[T value] { get => this.FindVertex(value); }
+
+        public IEnumerable<T> BreadthFirst(T value)
+        {
+            if (this[value] == null)
+                throw new Exception();
+            var visited = new HashSet<T>();
+            var q = new Queue<Vertex<T>>();
+            q.Enqueue(this[value]);
+            visited.Add(this[value].Value);
+            while(q.Count > 0)
+            {
+                var current = q.Dequeue();
+                foreach(var neighbor in current.Neighbors)
+                {
+                    if(visited.Contains(neighbor.Value))
+                    {
+                        visited.Add(neighbor.Value);
+                        q.Enqueue(neighbor);
+                    }
+                }
+                yield return current.Value;
+            }
+            foreach(var item in Vertices)
+            {
+                if (!visited.Contains(item.Value))
+                    yield return item.Value;
+            }
+        }
+
+        
     }
 }
